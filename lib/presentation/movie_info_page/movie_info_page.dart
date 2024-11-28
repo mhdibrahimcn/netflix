50992210d915ad58cbcdb2bef92c0f1de350fdc9
mhdibrahimcn <mhdcrew46@gmail.com>
2024-11-28 06:41:59 +0530

    added movie info bloc impl

--- lib/presentation/movie_info_page/movie_info_page.dart
+++ lib/presentation/movie_info_page/movie_info_page.dart
@@ -1,8 +1,7 @@
-import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:netflix/application/movie_info/movie_info_bloc.dart';
-import 'package:netflix/core/constants.dart';
+import 'package:youtube_player_flutter/youtube_player_flutter.dart';
 
 class MovieInfoPage extends StatelessWidget {
   final int movieId;
@@ -14,50 +13,69 @@ class MovieInfoPage extends StatelessWidget {
     // Fetch movie info when the widget is built
     WidgetsBinding.instance.addPostFrameCallback((_) {
       BlocProvider.of<MovieInfoBloc>(context)
-          .add(MovieInfoEvent.getMovieInfo(movieId));
+          .add(MovieInfoEvent.getMovieVideoLink(movieId));
     });
+
+    final Size size = MediaQuery.of(context).size;
 
     return BlocBuilder<MovieInfoBloc, MovieInfoState>(
       builder: (context, state) {
-        // Checking for different states of the bloc
         if (state.isLoading) {
           return const Center(
             child: CircularProgressIndicator(),
           );
-        } else if (state.isError) {
+        } else if (state.isError || state.youtubeVideoId.isEmpty) {
           return const Center(
-            child: Text('Failed to load movie info'),
+            child: Text('Failed to load movie info or no video available.'),
           );
         } else {
-          // Ensure state.movieInfo and its properties are not null
-          if (state.movieInfo != null && state.movieInfo!.posterPath != null) {
-            return Scaffold(
-              body: Column(
-                children: [
-                  Center(
-                    child: Container(
-                      width: 300,
-                      height: 300,
-                      decoration: BoxDecoration(
-                        image: DecorationImage(
-                          image: CachedNetworkImageProvider(
-                            '$imageAppendUrl${state.movieInfo!.backdropPath!}',
-                          ),
-                          fit: BoxFit
-                              .cover, // Adjust as per your UI requirements
+          return Scaffold(
+            body: ListView(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Container(
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(16.0),
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.grey.withOpacity(0.5),
+                          spreadRadius: 2,
+                          blurRadius: 7,
+                          offset:
+                              const Offset(0, 3), // changes position of shadow
                         ),
+                      ],
+                    ),
+                    child: ClipRRect(
+                      borderRadius: BorderRadius.circular(16.0),
+                      child: YoutubePlayerBuilder(
+                        player: YoutubePlayer(
+                          controller: YoutubePlayerController(
+                            initialVideoId: state.youtubeVideoId.first.key!,
+                            flags: const YoutubePlayerFlags(
+                              autoPlay: true,
+                              mute: false,
+                              captionLanguage: 'en',
+                            ),
+                          ),
+                          showVideoProgressIndicator: true,
+                          onReady: () {
+                            print('Player is ready.');
+                          },
+                          onEnded: (data) {
+                            print('Video ended.');
+                          },
+                        ),
+                        builder: (context, player) => player,
                       ),
                     ),
                   ),
-                  // Add more widgets here based on your UI design
-                ],
-              ),
-            );
-          } else {
-            return const Center(
-              child: Text('No poster available'),
-            );
-          }
+                ),
+                const Text('hvhgv')
+              ],
+            ),
+          );
         }
       },
     );
